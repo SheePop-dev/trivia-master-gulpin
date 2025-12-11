56cfb20ed243668a9fcc2fedef9830bffa6925b8
SheePop-dev <160767676+SheePop-dev@users.noreply.github.com>
2025-12-10 20:35:22 -0600

    Create fanmade-questions.js

---
+++ fanmade-questions.js
@@ -0,0 +1,24 @@
+// Questions should be in the format below
+// ["category", "question", "correct answer", "incorrect answer 1", "incorrect answer 2", "incorrect answer 3"],
+var fanmadeQuestionArray = [ 
+  ["Pokemon", "Which of the following pokemon evolves into Seviper", "Arbok", "Sableye", "Suicune", "Sneasel"],
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+];
