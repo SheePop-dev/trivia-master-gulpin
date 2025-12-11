f08fee020e3cbcbc8169beac9f01289b8fe86831
Greg <glergfrog220@gmail.com>
2025-12-11 22:11:46 +0000

    Update fanmade-questions.js
    No looking

--- fanmade-questions.js
+++ fanmade-questions.js
@@ -7,8 +7,14 @@ var fanmadeQuestionArray = [
   ["Greg", "Which dino is my favourite?", "Quetzalcoatlus", "T-rex", "Ankylosaurus", "Velociraptor"],
   ["Greg", "Would you still love me if i were a worm?", "Yes", "No", "Maybe", "67"],
   ["Greg", "What is the name of my little drawing", "Norbert", "Geoffery", "Franklin", "Fitzgerald"],
-  ["Greg", "Which of these is not my tag somewhere", "TheGregariousGreg", "RGregariousGreg", "Real_Gregarious_Greg", "NefariousNed"
-   
+  ["Greg", "Which of these is not my tag somewhere", "TheGregariousGreg", "RGregariousGreg", "Real_Gregarious_Greg", "NefariousNed"],
+  ["Greg", "As of rn, how many yt subs do i have?", "137", "153", "167", "126"],
+  ["Greg", "What is my prefered way of having an egg?", "Poached", "Sunnyside up", "Hard-boiled", "Scrambled"],
+  ["Greg", "I ran out of ideas, fuck you", "This one?", "This one?", "This one?", "This one?"],
+  ["Greg", "Which of these smash bros characters do I not like playing?", "Isabelle", "Hero", "Dr. Mario", "Little Mac"],
+  ["Greg", "What is my name?", "Greg", "Robert", "Calvin", "Hɔkʰ ˈtʼɤ̂ː"],
+  ["Greg", "What game have I done speedruns for?" "Legend of Zelda: Link to the Past", "Hong Kong 97", "Minecraft", "Super Mario 64"],
+  
 
 
 
