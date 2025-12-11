4b5ef14f96e3ad0e540e4204397b2ee300b31613
Greg <glergfrog220@gmail.com>
2025-12-11 21:56:03 +0000

    Update fanmade-questions.js
    No looking

--- fanmade-questions.js
+++ fanmade-questions.js
@@ -2,8 +2,13 @@
 // ["category", "question", "correct answer", "incorrect answer 1", "incorrect answer 2", "incorrect answer 3"],
 var fanmadeQuestionArray = [ 
   ["Pokemon", "Which of the following pokemon evolves into Seviper", "Arbok", "Sableye", "Suicune", "Sneasel"],
-
-
+  ["Greg", "Which of these Pokémon do i not have as a plush?", "Grookey", "Piplup", "Leafeon", "Quagsire"],
+  ["category", "question", "incorrect answer", "correct answer", "incorrect answer 2", "incorrect answer 3"],
+  ["Greg", "Which dino is my favourite?", "Quetzalcoatlus", "T-rex", "Ankylosaurus", "Velociraptor"],
+  ["Greg", "Would you still love me if i were a worm?", "Yes", "No", "Maybe", "67"],
+  ["Greg", "What is the name of my little drawing", "Norbert", "Geoffery", "Franklin", "Fitzgerald"],
+  ["Greg", "Which of these is not my tag somewhere", "TheGregariousGreg", "RGregariousGreg", "Real_Gregarious_Greg", "NefariousNed"
+   
 
 
 
